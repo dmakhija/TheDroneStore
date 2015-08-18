343669ce2c6f9acddc40d76575c3fc9036b16d66
Durga Makhija <dm.canada@yahoo.com>
2015-08-18 18:23:44 -0400

    wordpress ecommerce application

---
+++ wp-content/plugins/woocommerce-products-slider/readme.txt
@@ -0,0 +1,91 @@
+=== Woocommerce Products Slider ===
+	Contributors: paratheme
+	Donate link: http://paratheme.com
+	Tags: carousel, product, slider, woocommerce, carousel slider, product slider, woocommerce slider, Woocommerce Product slider
+	Requires at least: 3.8
+	Tested up to: 4.0
+	Stable tag: 1.2
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+	Fully responsive and mobile ready Carousel Slider for your woo-commerce product. unlimited slider anywhere via short-codes and easy admin setting.
+
+== Description ==
+
+
+A premium quality carousel slider to slide your woo-commerce product. easy to use via shortcode anywhere 
+
+
+### Woocommerce Products Slider by http://paratheme.com
+
+* [See the Live demo!&raquo;](http://product-slider.com)
+* [Watch Video Tutorial!&raquo;](https://www.youtube.com/watch?v=B0sOSp3h9fE)
+* [Upgrade to Premium!&raquo;](http://paratheme.com/items/woocommerce-product-slider-for-wordpress/)
+
+
+<strong>Plugin Features</strong><br />
+
+* Fully responsive and mobile ready.
+* Unlimited slider anywhere.
+* Query post from latest post, Older Published, Featured Items, by Only Year, by Month of a year, Taxonomy & Categories, Product id.
+* Two different Theme.
+* Custom number of slider items to query.
+* Custom number of  Slider Column Number.
+* Slider Items Thumbnail images Size selection.
+* Featured product marker.
+* Ribbons for slider.
+* Background Image for slider area.
+* Add to cart Background Color.
+* Add to cart Text Color.
+* Items Title Color
+* Items Title Font Size
+* Items Price Color
+* Items price Font Size
+* Slider Pagination Background Color
+* Slider Pagination Text Color
+
+<br />
+<strong>Video Tutorial Here.</strong>
+https://www.youtube.com/watch?v=B0sOSp3h9fE
+
+== Installation ==
+
+1. Install as regular WordPress plugin.<br />
+2. Go your plugin setting via WordPress Dashboard and find "<strong>Woocommerce Products Slider</strong>" activate it.<br />
+
+After activate plugin you will see "WCPS" menu at left side on WordPress dashboard click "New WCPS" and use the options field "Woocommerce Products Slider"<br />
+
+<br />
+<strong>How to use on page or post</strong><br />
+When slider options setup done please publish WCPS as like post or page<br />
+
+and then copy shortcode from top of <strong>Woo-commerce Products Slider Options</strong> `[wcps  id="1234" ]`<br />
+
+then paste this shortcode anywhere in your page to display slider<br />
+
+
+
+
+
+
+
+== Screenshots ==
+
+1. screenshot-1 Theme one
+2. screenshot-1 Theme two.
+3. screenshot-1 admin settings
+
+== Changelog ==
+
+
+	= 1.2 =
+    * 26/08/2014 javascripts conflit fixed.
+	* 26/08/2014 query post error fixed.
+
+	= 1.1 =
+    * 24/08/2014 Update plugin description.
+	* 24/08/2014 Update Live Demo.
+	* 24/08/2014 Added Video Tutorial.
+	
+	= 1.0 =
+    * 22/08/2014 Initial release.
---
+++ wp-content/themes/mystile/header.php
@@ -0,0 +1,130 @@
+<?php
+// File Security Check
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    die ( 'You do not have sufficient permissions to access this page!' );
+}
+?>
+<?php
+/**
+ * Header Template
+ *
+ * Here we setup all logic and XHTML that is required for the header section of all screens.
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+global $woo_options, $woocommerce;
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="<?php if ( $woo_options['woo_boxed_layout'] == 'true' ) echo 'boxed'; ?> 
+				<?php if (!class_exists('woocommerce')) echo 'woocommerce-deactivated'; ?>">
+<head>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title><?php woo_title(''); ?></title>
+<?php woo_meta(); ?>
+
+<?php
+//echo bloginfo( 'stylesheet_url' );
+//echo get_stylesheet_uri();
+//echo get_stylesheet_directory_uri();
+//echo get_template_directory_uri();
+?>
+<!-- the woocommerce mystile css -->
+<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" media="screen" /> 
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<!-- added favicon reference-->
+
+
+<!-- Bootstrap header starts -->
+
+<!-- get_stylesheet_directory_uri(); Retrieve stylesheet directory URI for the current theme/child theme. Does not contain a trailing slash. returns a -->
+<!-- properly-formed URI; in other words, it will be a web-address (starting with http:// or https:// for SSL). -->
+
+<link href="<?php echo get_stylesheet_uri();?>/custom.css" rel="stylesheet" media="screen" />	
+<link href="<?php echo get_template_directory_uri();?>/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<link href="http://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css" />
+<!-- Bootstrap header ends -->
+
+<?php
+	wp_head();
+	woo_head();
+?>
+
+</head>
+
+<body <?php body_class(); ?>>
+<?php woo_top(); ?>
+
+<div id="wrapper">
+
+	<div id="top">
+		<nav class="col-full" role="navigation">
+			<?php if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'top-menu' ) ) { ?>
+			<?php wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'top-nav', 'menu_class' => 'nav fl', 'theme_location' => 'top-menu' ) ); ?>
+			<?php } ?>
+			<?php
+				if ( class_exists( 'woocommerce' ) ) {
+					echo '<ul class="nav wc-nav">';
+					woocommerce_cart_link();
+					echo '<li class="checkout"><a href="'.esc_url($woocommerce->cart->get_checkout_url()).'">'.__('Checkout','woothemes').'</a></li>';
+					echo get_search_form();
+					echo '</ul>';
+				}
+			?>
+		</nav>
+	</div><!-- /#top -->
+
+
+
+    <?php woo_header_before(); ?>
+
+	<header id="header" class="col-full">
+
+
+
+	    <hgroup>
+
+	    	 <?php
+			    $logo = esc_url( get_template_directory_uri() . '/images/logo.png' );
+				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' ) { $logo = $woo_options['woo_logo']; }
+				if ( isset( $woo_options['woo_logo'] ) && $woo_options['woo_logo'] != '' && is_ssl() ) { $logo = preg_replace("/^http:/", "https:", $woo_options['woo_logo']); }
+			?>
+			<?php if ( ! isset( $woo_options['woo_texttitle'] ) || $woo_options['woo_texttitle'] != 'true' ) { ?>
+			    <a id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr( get_bloginfo( 'description' ) ); ?>">
+			    	<img src="<?php echo $logo; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+			    </a>
+		    <?php } ?>
+
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1> 
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h3 class="nav-toggle"><a href="#navigation"><mark class="websymbols">&#178;</mark> <span><?php _e('Navigation', 'woothemes'); ?></span></a></h3>
+
+		</hgroup>
+
+        <?php woo_nav_before(); ?>
+
+		<nav id="navigation" class="col-full" role="navigation">
+
+			<?php
+			if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'primary-menu' ) ) 
+			{
+				wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fr', 'theme_location' => 'primary-menu' ) );
+			} else {
+			?>
+	        <ul id="main-nav" class="nav fl">
+				<?php if ( is_page() ) $highlight = 'page_item'; else $highlight = 'page_item current_page_item'; ?>
+				<li class="<?php echo $highlight; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e( 'Home', 'woothemes' ); ?></a></li>
+				<?php wp_list_pages( 'sort_column=menu_order&depth=6&title_li=&exclude=' ); ?>
+			</ul><!-- /#nav -->
+	        <?php } ?>
+
+		</nav><!-- /#navigation -->
+
+		<?php woo_nav_after(); ?>
+
+	</header><!-- /#header -->
+
+	<?php woo_content_before(); ?>
